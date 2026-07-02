7cd590ffd1e49823b49c621795a66852e5893921
heiyanbin <heiyanbin@163.com>
2026-07-02 18:29:21 +0800

    set Chrome headless to True

--- conf.py
+++ conf.py
@@ -3,4 +3,4 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.resolve()
 XHS_SERVER = "http://127.0.0.1:11901"
 LOCAL_CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"   # change me necessary！ for example C:/Program Files/Google/Chrome/Application/chrome.exe
-LOCAL_CHROME_HEADLESS = False
+LOCAL_CHROME_HEADLESS = True
